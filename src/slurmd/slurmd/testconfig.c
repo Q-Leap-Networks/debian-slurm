32d1dae8452dad1eca4aae1b1fa0c73efcc0477d
Gennaro Oliva <oliva.g@na.icar.cnr.it>
2008-04-01 15:27:08 +0200

    Imported Debian patch 1.2.25-2

--- src/slurmd/slurmd/testconfig.c
+++ src/slurmd/slurmd/testconfig.c
@@ -1,1 +1,1 @@
-char *slurm_stepd_path = "/home/oliva/debs/slurm-llnl/1.2.25/slurm-1.2.25/src/slurmd/slurmstepd/slurmstepd";
+char *slurm_stepd_path = "/home/oliva/debs/slurm-llnl/1.2.25/slurm-llnl-1.2.5/src/slurmd/slurmstepd/slurmstepd";
